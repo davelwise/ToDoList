e5b3247937211a1e94effff8c6114dfb5848cb9c
David Wise <davelwise@msn.com>
2013-11-11 22:44:24 -0600

    Testing

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by David Wise on 11/11/13.
+//  Copyright (c) 2013 Wise Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
